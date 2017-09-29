6a1b7dffbaa7043dac6c67340cb13ed424d0db58
Kyle Andrews <kylemandrews@gmail.com>
2017-09-29 15:40:18 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(x)
+  nums = *(1...x)
+    # binding.pry
+  nums.select do |num|
+
+    num % 3 == 0 || num % 5 == 0
+  end
+end
+
+def sum_multiples(x)
+  collect_multiples(x).inject(0){|sum, x| sum + x}
+end
